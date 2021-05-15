5283ecc8f56015a67ea6538eef8dd260baa18cd8
Kes001 <procenkok@yandex.ru>
2021-05-15 20:44:34 +0500

    Исправление warning

--- Use_sosed's_library.cpp
+++ Use_sosed's_library.cpp
@@ -9,14 +9,14 @@ int main ()
     for (int t = 0; t <= 400; t++)
         {
         txBegin ();
-        txClear    ();
-        Background ();
 
+        txClear      ();
+        Background   ();
         SolnceDraw   (100 + t, 80, 1 + t % 50);
         Oblaka       (t);
         RazmetkaDraw (510, 0);
         Lesopolosa   (0);
-        MoskvichDraw (1600 - t*2.5, 400, 0, 0, 0, 0, 1);
+        MoskvichDraw (ROUND(1600 - t*2.5), 400, 0, 0, 0, 0, 1);
         SemerkaDraw  (-800 + t*2, 570);
 
         MadamDrawProfil (0 + t, 490, TX_RED, 0, 0, 0 + (t/30)%2, 1 - (t/30)%2, 1, 1);
@@ -30,6 +30,7 @@ int main ()
     for (int t = 0; t <= 5; t++)
         {
         txBegin ();
+
         txClear      ();
         Background   ();
         SolnceDraw   (500 + t, 80, 1 + t % 50);
@@ -42,13 +43,15 @@ int main ()
 
         MadamDrawProfil (400, 490, TX_RED, 0, 0, t%2, t%2, 1, 1*golova);
         golova *= -1;
-        txSleep (200);
+
+        txSleep (300);
         txEnd   ();
         }
 
     for (int t = 0; t <= 75; t++)
         {
         txBegin ();
+
         txClear      ();
         Background   ();
         SolnceDraw   (510 + t, 80, 1 + t % 50);
@@ -61,6 +64,7 @@ int main ()
 
         MadamDrawProfil (400 - t*2, 490, TX_RED, 0, 0, 0 + (t/30)%2, 1 - (t/30)%2, 1, -1);
         golova *= -1;
+
         txSleep (20);
         txEnd   ();
         }
@@ -68,6 +72,7 @@ int main ()
     for (int t = 0; t <= 150; t++)
         {
         txBegin ();
+
         txClear      ();
         Background   ();
         SolnceDraw   (590 + t, 80, 1 + t % 50);
@@ -78,7 +83,6 @@ int main ()
         SemerkaDraw  (0 + t*5, 570);
         MoskvichDraw (600, 400, -30 - t/5, 0, 0, (t/10)%2, 1);
 
-        golova *= -1;
         txSleep (10);
         txEnd   ();
         }
